cca4f13f4c7ef79bf23d5a1fc6d03cd0224f186a
Long <KamiKun369@gmail.com>
2025-09-24 17:35:31 +0700

    explore.c

--- explore.c
+++ explore.c
@@ -1,1 +1,27 @@
-// Write some thing like explore(Game *game) here
+#include <stdio.h>
+#include "game_data.h"
+void explorer(Game *game, Config *config) {
+    int choice;
+    printf("=== Exploration Mode ===\n");
+    while (1) {
+        printf("\nWhat would you like to do?\n");
+        printf("1. Move\n");
+        printf("2. Shop\n");
+        printf("3. Exit exploration\n");
+        printf("Choice: ");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1:
+                printf("You move to a new location...\n");
+                break;
+            case 2:
+                printf("You enter the shop...\n");
+                break;
+            case 3:
+                printf("Exiting exploration...\n");
+                return;
+            default:
+                printf("Invalid choice. Try again.\n");
+        }
+    }
+}
