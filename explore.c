8d48806c2734b1cfed0ad258af74f3251cd79576
Long <KamiKun369@gmail.com>
2025-09-24 17:35:31 +0700

    explore.c

--- explore.c
+++ explore.c
@@ -1,18 +1,27 @@
-#include <stdlib.h>
-#include "game_object.h"
-
-void explore(Game * game) {
-    int i;
-    for (i = 0; i < game->config.explorationTurn;i++) {
-        printf("Exploring...");
-        int chance = rand()%100;
-        // Todo fix the config of exploring 
-        if (chance > game->config.combatChance) {
-            createCombat(game);
-        } else if (chance > game->config.combatChance + game->config.tradeChance) {
-            openShop(game);
-        } else { //if (chance )
-            createMinigame(game);
+#include <stdio.h>
+#include "game_data.h"
+void explorer(Game *game, Config *config) {
+    int choice;
+    printf("=== Exploration Mode ===\n");
+    while (1) {
+        printf("\nWhat would you like to do?\n");
+        printf("1. Move\n");
+        printf("2. Shop\n");
+        printf("3. Exit exploration\n");
+        printf("Choice: ");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1:
+                printf("You move to a new location...\n");
+                break;
+            case 2:
+                printf("You enter the shop...\n");
+                break;
+            case 3:
+                printf("Exiting exploration...\n");
+                return;
+            default:
+                printf("Invalid choice. Try again.\n");
         }
     }
 }
