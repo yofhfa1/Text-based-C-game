cc362fc2c481ffd2dcdc2c01ab8b9410857272c1
ktokto313 <kto3132006@gmail.com>
2025-09-29 08:56:51 +0700

    Make explore.c

--- explore.c
+++ explore.c
@@ -1,1 +1,18 @@
-// Write some thing like explore(Game *game) here
+#include <stdlib.h>
+#include "game_object.h"
+
+void explore(Game * game) {
+    int i;
+    for (i = 0; i < game->config.explorationTurn;i++) {
+        printf("Exploring...");
+        int chance = rand()%100;
+        // Todo fix the config of exploring 
+        if (chance > game->config.combatChance) {
+            createCombat(game);
+        } else if (chance > game->config.combatChance + game->config.tradeChance) {
+            openShop(game);
+        } else { //if (chance )
+            createMinigame(game);
+        }
+    }
+}
